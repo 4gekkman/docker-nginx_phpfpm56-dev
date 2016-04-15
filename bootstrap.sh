c8f0b78a02c0b62c43483fdbdac5e278b2b9c979
4gekkman <4gekkman@gmail.com>
2016-04-15 18:00:55 +0300

    Autosave

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
   # 1. Скопировать resolv с правильным IP DNS-сервера для postfix
-  cp -rf /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+  cp -f /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
 
   # 2. Подготовить nano к работе
   export TERM=xterm
